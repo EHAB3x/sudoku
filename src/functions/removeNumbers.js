4a7c8648a46dac3e40a44dc938431a89557dce05
EHAB3x <ehabmah6969@gmail.com>
2024-05-03 02:06:56 +0200

    remove unnecessary comments from the code

--- src/functions/removeNumbers.js
+++ src/functions/removeNumbers.js
@@ -1,5 +1,5 @@
 export default function removeNumbers(board, difficulty) {
-    // Calculate the number of cells to remove based on difficulty
+
     let cellsToRemove = difficulty * 25;
     while (cellsToRemove > 0) {
         let row = Math.floor(Math.random() * 9);
--- src/functions/solveSudoku.js
+++ src/functions/solveSudoku.js
@@ -8,35 +8,27 @@ export default function solveSudokuDFS(board) {
 }
 
 function dfs(board, row, col) {
-    // If all cells are filled, the puzzle is solved
     if (row === 9) {
         return board;
     }
 
-    // Calculate the next cell coordinates
     const nextRow = col === 8 ? row + 1 : row;
     const nextCol = col === 8 ? 0 : col + 1;
 
-    // If the current cell is not empty, move to the next cell
     if (board[row][col] !== 0) {
         return dfs(board, nextRow, nextCol);
     }
 
-    // Try placing numbers from 1 to 9
     for (let num = 1; num <= 9; num++) {
-        // Check if the number can be placed in the current cell
         if (isValid(board, row, col, num)) {
-            // Place the number in the current cell
             board[row][col] = num;
-            // Recursively try to solve the puzzle with the current number placed
             if (dfs(board, nextRow, nextCol)) {
-                return board; // If the puzzle is solved, return true
+                return board;
             }
-            // If the current number doesn't lead to a solution, backtrack and try the next number
             board[row][col] = 0;
         }
     }
 
-    return false; // If no number can be placed in the current cell, backtrack
+    return false;
 }
 
